e27e59b63beb828cfc85d3e24b9c7724fee5ba43
claeis <claeis@users.noreply.github.com>
2017-03-08 10:12:23 +0100

    itf: validate TID per table

--- src/ch/ehi/ili2db/base/Ili2db.java
+++ src/ch/ehi/ili2db/base/Ili2db.java
@@ -586,6 +586,7 @@ public class Ili2db {
 						if(function!=config.FC_DELETE){
 							  EhiLogger.logState("data <"+inputFilename+">");
 								if(isItfFilename(inputFilename)){
+									config.setValue(ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_OIDPERTABLE, ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_OIDPERTABLE_DO);
 									if(config.getDoItfLineTables()){
 										ioxReader=new ItfReader(new java.io.File(inputFilename));
 										((ItfReader)ioxReader).setModel(td);		
@@ -1817,6 +1818,7 @@ public class Ili2db {
 					}else{
 						ioxWriter=new ItfWriter(outfile,td);
 					}
+					config.setValue(ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_OIDPERTABLE, ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_OIDPERTABLE_DO);
 				}else if(ext!=null && ext.equals("gml")){
 					ioxWriter=new Iligml10Writer(outfile,td);
 				}else{
