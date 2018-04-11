4a6bf9b10a7f7bc9a92d76939ee41291a23b2b2c
rhiltbrunner <rh@eisenhutinformatik.ch>
2018-04-11 16:21:52 +0200

    added: reading by XtfReader of ili2fgdb export tests and added appropriate assertions, because the content of the exported XTF file was not tested until yet.

--- ili2fgdb/test/java/ch/ehi/ili2fgdb/MultisurfaceTest.java
+++ ili2fgdb/test/java/ch/ehi/ili2fgdb/MultisurfaceTest.java
@@ -1,29 +1,48 @@
 package ch.ehi.ili2fgdb;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import ch.ehi.basics.logging.EhiLogger;
 import ch.ehi.fgdb4j.Fgdb4j;
-import ch.ehi.ili2db.base.DbUrlConverter;
 import ch.ehi.ili2db.base.Ili2db;
-import ch.ehi.ili2db.base.Ili2dbException;
 import ch.ehi.ili2db.gui.Config;
-import ch.ehi.ili2db.mapping.NameMapping;
 import ch.ehi.ili2fgdb.jdbc.FgdbDriver;
+import ch.interlis.iom.IomObject;
+import ch.interlis.iom_j.xtf.XtfReader;
+import ch.interlis.iox.EndBasketEvent;
+import ch.interlis.iox.EndTransferEvent;
+import ch.interlis.iox.IoxEvent;
+import ch.interlis.iox.ObjectEvent;
+import ch.interlis.iox.StartBasketEvent;
+import ch.interlis.iox.StartTransferEvent;
 
 public class MultisurfaceTest {
-    String fgdbFileName="test/data/MultiSurface/MultiSurface.gdb";
-	Connection jdbcConnection=null;
+	
+	private static final String TEST_OUT="test/data/MultiSurface/";
+    private static final String FGDBFILENAME=TEST_OUT+"MultiSurface.gdb";
+	private Connection jdbcConnection=null;
+	
+	public Config initConfig(String xtfFilename,String logfile) {
+		Config config=new Config();
+		new ch.ehi.ili2fgdb.FgdbMain().initConfig(config);
+		config.setDbfile(FGDBFILENAME);
+		config.setDburl(FgdbDriver.BASE_URL+FGDBFILENAME);
+		if(logfile!=null){
+			config.setLogfile(logfile);
+		}
+		config.setXtffile(xtfFilename);
+		if(xtfFilename!=null && Ili2db.isItfFilename(xtfFilename)){
+			config.setItfTransferfile(true);
+		}
+		return config;
+	}
+	
 	@After
 	public void endDb() throws Exception
 	{
@@ -31,33 +50,14 @@ public class MultisurfaceTest {
 			jdbcConnection.close();
 		}
 	}
-	public Config initConfig(String xtfFilename,String logfile) {
-		Config config=new Config();
-		new ch.ehi.ili2fgdb.FgdbMain().initConfig(config);
-		
-		
-		config.setDbfile(fgdbFileName);
-		config.setDburl(FgdbDriver.BASE_URL+fgdbFileName);
-		if(logfile!=null){
-			config.setLogfile(logfile);
-		}
-
-
-		config.setXtffile(xtfFilename);
-		if(xtfFilename!=null && Ili2db.isItfFilename(xtfFilename)){
-			config.setItfTransferfile(true);
-		}
-		return config;
-		
-	}
 	
 	@Test
 	public void importIli() throws Exception
 	{
-	    File fgdbFile=new File(fgdbFileName);
+	    File fgdbFile=new File(FGDBFILENAME);
 	    Fgdb4j.deleteFileGdb(fgdbFile);
 	    Class driverClass = Class.forName(FgdbDriver.class.getName());
-		File data=new File("test/data/MultiSurface/MultiSurface2.ili");
+		File data=new File(TEST_OUT,"MultiSurface2.ili");
 		Config config=initConfig(data.getPath(),data.getPath()+".log");
 		config.setFunction(Config.FC_SCHEMAIMPORT);
 		config.setCreateFk(config.CREATE_FK_YES);
@@ -71,16 +71,16 @@ public class MultisurfaceTest {
 		config.setInheritanceTrafo(null);
 		//Ili2db.readSettingsFromDb(config);
 		Ili2db.run(config,null);
-		
 	}
+	
 	@Test
 	public void importXtf() throws Exception
 	{
 		EhiLogger.getInstance().setTraceFilter(false);
-	    File fgdbFile=new File(fgdbFileName);
+	    File fgdbFile=new File(FGDBFILENAME);
 	    Fgdb4j.deleteFileGdb(fgdbFile);
 	    Class driverClass = Class.forName(FgdbDriver.class.getName());
-		File data=new File("test/data/MultiSurface/MultiSurface2a.xtf");
+		File data=new File(TEST_OUT,"MultiSurface2a.xtf");
 		Config config=initConfig(data.getPath(),data.getPath()+".log");
 		config.setFunction(Config.FC_IMPORT);
 		config.setCreateFk(config.CREATE_FK_YES);
@@ -99,15 +99,17 @@ public class MultisurfaceTest {
 			EhiLogger.logError(ex);
 			Assert.fail();
 		}
-		
 	}
+	
 	@Ignore("unexpected nullPointerException in methode: ch.ehi.ili2db.toxtf.ToXtfRecordConverter.addAttrValue")
 	@Test
 	public void exportXtf() throws Exception
 	{
-		importXtf();
+		{
+			importXtf();
+		}
 		EhiLogger.getInstance().setTraceFilter(false);
-	    File fgdbFile=new File(fgdbFileName);
+	    File fgdbFile=new File(FGDBFILENAME);
 	    //Fgdb4j.deleteFileGdb(fgdbFile);
 	    Class driverClass = Class.forName(FgdbDriver.class.getName());
 		File data=new File("test/data/Simple/Simple23a-out.xtf");
@@ -121,6 +123,80 @@ public class MultisurfaceTest {
 			EhiLogger.logError(ex);
 			Assert.fail();
 		}
-		
+		{
+			XtfReader reader=new XtfReader(data);
+
+			assertTrue(reader.read() instanceof StartTransferEvent);
+			assertTrue(reader.read() instanceof StartBasketEvent);
+			IoxEvent event=reader.read();
+			{
+				assertTrue(event instanceof ObjectEvent);
+				IomObject iomObj=((ObjectEvent)event).getIomObject();
+				String attrtag=iomObj.getobjecttag();
+				assertEquals("MultiLine2.TestA.ClassA1",attrtag);
+				
+				IomObject multisurface=iomObj.getattrobj("geom", 0);
+				assertTrue(multisurface.getattrvaluecount("geom")==2);
+				{	
+					IomObject surface=multisurface.getattrobj("surface", 0);
+					IomObject boundary=surface.getattrobj("boundary", 0);
+					IomObject polylineObj=boundary.getattrobj("polyline", 0);
+					IomObject sequence=polylineObj.getattrobj("sequence", 0);
+					{
+						IomObject segment=sequence.getattrobj("segment", 0);
+						assertTrue(segment.getattrvalue("C1").equals("600001.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205001.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 1);
+						assertTrue(segment.getattrvalue("C1").equals("600020.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205015.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 2);
+						assertTrue(segment.getattrvalue("A1").equals("600010.0"));
+						assertTrue(segment.getattrvalue("A2").equals("205018.0"));
+						assertTrue(segment.getattrvalue("C1").equals("600001.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205015.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 3);
+						assertTrue(segment.getattrvalue("C1").equals("600001.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205001.0"));
+					}
+				}
+				IomObject multisurface2=iomObj.getattrobj("geom", 1);
+				{
+					IomObject surface=multisurface2.getattrobj("surface", 0);
+					IomObject boundary=surface.getattrobj("boundary", 0);
+					IomObject polylineObj=boundary.getattrobj("polyline", 0);
+					IomObject sequence=polylineObj.getattrobj("sequence", 0);
+					{
+						IomObject segment=sequence.getattrobj("segment", 0);
+						assertTrue(segment.getattrvalue("C1").equals("600005.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205005.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 1);
+						assertTrue(segment.getattrvalue("C1").equals("600010.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205010.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 2);
+						assertTrue(segment.getattrvalue("A1").equals("600007.0"));
+						assertTrue(segment.getattrvalue("A2").equals("205009.0"));
+						assertTrue(segment.getattrvalue("C1").equals("600005.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205010.0"));
+					}
+					{
+						IomObject segment=sequence.getattrobj("segment", 3);
+						assertTrue(segment.getattrvalue("C1").equals("600005.0"));
+						assertTrue(segment.getattrvalue("C2").equals("205005.0"));
+					}
+				}
+				assertTrue(reader.read() instanceof EndBasketEvent);
+				assertTrue(reader.read() instanceof EndTransferEvent);
+			}
+		}
 	}
 }
