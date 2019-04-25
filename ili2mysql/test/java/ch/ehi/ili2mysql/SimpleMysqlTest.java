a0c7d1c6eda4e1f2c0bc6c5bdbe8035d0a8e9ef6
claeis <ce@eisenhutinformatik.ch>
2019-04-25 14:59:05 +0200

    fixed failing tests

--- ili2mysql/test/java/ch/ehi/ili2mysql/SimpleMysqlTest.java
+++ ili2mysql/test/java/ch/ehi/ili2mysql/SimpleMysqlTest.java
@@ -1,21 +1,16 @@
 package ch.ehi.ili2mysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import ch.ehi.ili2db.AbstractTestSetup;
-import ch.ehi.ili2db.base.Ili2db;
-import ch.ehi.ili2db.gui.Config;
 
 public class SimpleMysqlTest extends ch.ehi.ili2db.SimpleTest {
 	
-    String dburl=System.getProperty("dburl"); 
-    String dbuser=System.getProperty("dbusr");
-    String dbpwd=System.getProperty("dbpwd");
     @Override
     protected AbstractTestSetup createTestSetup() {
+        
+        String dburl=System.getProperty("dburl"); 
+        String dbuser=System.getProperty("dbusr");
+        String dbpwd=System.getProperty("dbpwd");
+        
         return new MysqlTestSetup(dburl,dbuser,dbpwd);
     } 
 
