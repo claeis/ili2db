b0cd41c4c1aaad7f78c7a8baa68630ac62278d1e
claeis <ce@eisenhutinformatik.ch>
2021-03-09 17:04:25 +0100

    ili2h2gis: add schema support (#383)

--- ili2h2gis/test/java/ch/ehi/ili2h2gis/H2gisTestSetup.java
+++ ili2h2gis/test/java/ch/ehi/ili2h2gis/H2gisTestSetup.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import ch.ehi.ili2db.AbstractTestSetup;
 import ch.ehi.ili2db.base.Ili2db;
@@ -11,11 +12,17 @@ import ch.ehi.ili2db.gui.Config;
 
 public class H2gisTestSetup extends AbstractTestSetup {
     
+    private String dbschema;
     private String h2gisFilename;
     private String dburl;
+    
     public H2gisTestSetup(String gpkgFilename,String dburl) {
+        this(gpkgFilename,dburl,null);
+    }
+    public H2gisTestSetup(String gpkgFilename,String dburl,String dbschema) {
         this.h2gisFilename=gpkgFilename;
         this.dburl=dburl;
+        this.dbschema=dbschema;
     }
     @Override
     public Config initConfig(String xtfFilename,String logfile) {
@@ -23,6 +30,9 @@ public class H2gisTestSetup extends AbstractTestSetup {
         new ch.ehi.ili2h2gis.H2gisMain().initConfig(config);
         config.setDbfile(h2gisFilename);
         config.setDburl(dburl);
+        if(dbschema!=null){
+            config.setDbschema(dbschema);
+        }
         if(logfile!=null){
             config.setLogfile(logfile);
         }
@@ -32,23 +42,51 @@ public class H2gisTestSetup extends AbstractTestSetup {
         }
         return config;
     }
+    @Override
+    public String prefixName(String name) {
+        if(dbschema==null) {
+            return name;
+        }
+        return dbschema+"."+name;
+    }
+    @Override
+    public String getSchema() {
+        return dbschema;
+    }
     
     @Override
-    public void resetDb() {
-        
-        // test.mv.db
-        // test.newFile
-        // test.tempFile
-        // test.h2.db
-        // test.lock.db 
-        // test.trace.db 
-        for(String ext:new String[] {".mv.db",".newFile",".tempFile",".h2.db",".lock.db",".trace.db"}) {
-            File dbfile=new File(h2gisFilename+ext);
-            if(dbfile.exists()){ 
-                File file = new File(dbfile.getAbsolutePath());
-                file.delete();
+    public void resetDb() throws SQLException {
+
+        if(dbschema!=null) {
+            Connection jdbcConnection=createConnection();
+            try {
+                Statement stmt=jdbcConnection.createStatement();
+                try {
+                    stmt.execute("DROP SCHEMA IF EXISTS "+dbschema+" CASCADE");
+                }finally {
+                    stmt.close();
+                    stmt=null;
+                }
+            }finally {
+                jdbcConnection.close();
+                jdbcConnection=null;
+            }
+        }else {
+            // test.mv.db
+            // test.newFile
+            // test.tempFile
+            // test.h2.db
+            // test.lock.db 
+            // test.trace.db 
+            for(String ext:new String[] {".mv.db",".newFile",".tempFile",".h2.db",".lock.db",".trace.db"}) {
+                File dbfile=new File(h2gisFilename+ext);
+                if(dbfile.exists()){ 
+                    File file = new File(dbfile.getAbsolutePath());
+                    file.delete();
+                }
             }
         }
+        
     }
     @Override
     public Connection createDbSchema() throws SQLException {
@@ -69,6 +107,9 @@ public class H2gisTestSetup extends AbstractTestSetup {
     @Override
     public void initConfig(Config config) {
         new ch.ehi.ili2h2gis.H2gisMain().initConfig(config);
+        if(dbschema!=null) {
+            config.setDbschema(dbschema);
+        }
     }
     @Override
     public boolean supportsCompoundGeometry(){
