ff403877518a8f85d6ba0c3b9487d4a462a1b4fc
Ruggero Botteon <botteon@puzzle.ch>
2021-08-19 15:24:13 +0200

    #334: fix model name, use an xtf with 1000 entries, check import size

--- test/java/ch/ehi/ili2db/BatchTest.java
+++ test/java/ch/ehi/ili2db/BatchTest.java
@@ -12,38 +12,39 @@ import java.sql.Statement;
 
 public abstract class BatchTest {
 
-    protected static final String TEST_OUT="test/data/BatchSize/";
-    protected AbstractTestSetup setup=createTestSetup();
-    protected abstract AbstractTestSetup createTestSetup() ;
+    protected static final String TEST_OUT = "test/data/BatchSize/";
+    protected AbstractTestSetup setup = createTestSetup();
+
+    protected abstract AbstractTestSetup createTestSetup();
 
     @Test
-    public void importXtf() throws Exception
-    {
+    public void importXtf() throws Exception {
         //EhiLogger.getInstance().setTraceFilter(false);
         setup.resetDb();
-        File data=new File(TEST_OUT,"BatchSize.xtf");
-        Config config=setup.initConfig(data.getPath(),data.getPath()+".log");
+        File data = new File(TEST_OUT, "BatchSize.xtf");
+        Config config = setup.initConfig(data.getPath(), data.getPath() + ".log");
         config.setFunction(Config.FC_IMPORT);
         config.setDoImplicitSchemaImport(true);
         config.setTidHandling(Config.TID_HANDLING_PROPERTY);
         config.setImportTid(true);
         config.setCreateNumChecks(true);
         config.setBatchSize(50);
-        Ili2db.run(config,null);
+        Ili2db.run(config, null);
 
         Connection jdbcConnection = setup.createConnection();
-        try{
-            Statement stmt=jdbcConnection.createStatement();
-            Assert.assertTrue(stmt.execute("SELECT ST_X(lcoord),ST_Y(lcoord) FROM "+setup.prefixName("ClassKoord2")+" WHERE t_ili_tid = 'Coord2.1'"));
+        try {
+            Statement stmt = jdbcConnection.createStatement();
+            Assert.assertTrue(stmt.execute("SELECT * FROM " + setup.getSchema() + ".classa1"));
             {
-                ResultSet rs=stmt.getResultSet();
-                Assert.assertTrue(rs.next());
-                Assert.assertEquals(2460001.000,rs.getDouble(1),0.00005);
-                Assert.assertEquals(1045001.000,rs.getDouble(2),0.00005);
+                ResultSet rs = stmt.getResultSet();
+                int count = 0;
+                while (rs.next()) {
+                    count++;
+                }
+                Assert.assertEquals(1000, count);
             }
-        }finally {
+        } finally {
             jdbcConnection.close();
-            jdbcConnection=null;
         }
     }
 
