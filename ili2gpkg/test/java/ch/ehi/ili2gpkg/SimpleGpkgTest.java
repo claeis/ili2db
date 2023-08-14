05827da67f714df8b9a7eb3cf45420902f08faae
claeis <ce@eisenhutinformatik.ch>
2023-08-14 16:14:56 +0200

    verify gpkg_contents

--- ili2gpkg/test/java/ch/ehi/ili2gpkg/SimpleGpkgTest.java
+++ ili2gpkg/test/java/ch/ehi/ili2gpkg/SimpleGpkgTest.java
@@ -1,5 +1,12 @@
 package ch.ehi.ili2gpkg;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Assert;
+
 import ch.ehi.ili2db.AbstractTestSetup;
 import ch.ehi.ili2db.SimpleTest;
 
@@ -10,6 +17,34 @@ public class SimpleGpkgTest extends SimpleTest {
     @Override
     protected AbstractTestSetup createTestSetup() {
         return new GpkgTestSetup(GPKGFILENAME,DBURL);
+    }
+    @Override
+    protected void validateImportIliCoord() throws Exception {
+        super.validateImportIliCoord();
+        Connection jdbcConnection=null;
+        Statement stmt=null;
+        try {
+            jdbcConnection = setup.createConnection();
+            stmt=jdbcConnection.createStatement();
+            ResultSet rs=stmt.executeQuery("SELECT table_name FROM gpkg_contents");
+            while(rs.next()) {
+                Assert.assertEquals("classa1",rs.getString(1));
+            }
+            rs=stmt.executeQuery("SELECT table_name,column_name FROM gpkg_geometry_columns");
+            while(rs.next()) {
+                Assert.assertEquals("classa1",rs.getString(1));
+                Assert.assertEquals("attr2",rs.getString(2));
+            }
+            
+        }finally {
+            if(stmt!=null) {
+                stmt.close();
+            }
+            if(jdbcConnection!=null) {
+                jdbcConnection.close();
+            }
+        }
+        
     }
 	
 	
