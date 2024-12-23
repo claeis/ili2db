f090890924ff8b9d36e4370a3373e15892c8b954
Stefan Ziegler <edi.gonzales@gmail.com>
2024-12-23 18:23:26 +0100

    start with SimpleDuckDBTest

--- ili2duckdb/test/java/ch/ehi/ili2duckdb/DuckDBTestSetup.java
+++ ili2duckdb/test/java/ch/ehi/ili2duckdb/DuckDBTestSetup.java
@@ -10,112 +10,114 @@ import ch.ehi.ili2db.AbstractTestSetup;
 import ch.ehi.ili2db.base.Ili2db;
 import ch.ehi.ili2db.gui.Config;
 
-public class DuckDBTestSetup /*extends AbstractTestSetup */{
+public class DuckDBTestSetup extends AbstractTestSetup {
     
-//    private String dbschema;
-//    private String h2gisFilename;
-//    private String dburl;
-//    
-//    public DuckDBTestSetup(String gpkgFilename,String dburl) {
-//        this(gpkgFilename,dburl,null);
-//    }
-//    public DuckDBTestSetup(String gpkgFilename,String dburl,String dbschema) {
-//        this.h2gisFilename=gpkgFilename;
-//        this.dburl=dburl;
-//        this.dbschema=dbschema;
-//    }
-//    @Override
-//    public Config initConfig(String xtfFilename,String logfile) {
-//        Config config=new Config();
-//        new ch.ehi.ili2h2gis.H2gisMain().initConfig(config);
-//        config.setDbfile(h2gisFilename);
-//        config.setDburl(dburl);
-//        if(dbschema!=null){
-//            config.setDbschema(dbschema);
-//        }
-//        if(logfile!=null){
-//            config.setLogfile(logfile);
-//        }
-//        config.setXtffile(xtfFilename);
-//        if(xtfFilename!=null && Ili2db.isItfFilename(xtfFilename)){
-//            config.setItfTransferfile(true);
-//        }
-//        return config;
-//    }
-//    @Override
-//    public String prefixName(String name) {
-//        if(dbschema==null) {
-//            return name;
-//        }
-//        return dbschema+"."+name;
-//    }
-//    @Override
-//    public String getSchema() {
-//        return dbschema;
-//    }
-//    
-//    @Override
-//    public void resetDb() throws SQLException {
-//
-//        if(dbschema!=null) {
-//            Connection jdbcConnection=createConnection();
-//            try {
-//                Statement stmt=jdbcConnection.createStatement();
-//                try {
-//                    stmt.execute("DROP SCHEMA IF EXISTS "+dbschema+" CASCADE");
-//                }finally {
-//                    stmt.close();
-//                    stmt=null;
-//                }
-//            }finally {
-//                jdbcConnection.close();
-//                jdbcConnection=null;
-//            }
-//        }
-//        {
-//            // test.mv.db
-//            // test.newFile
-//            // test.tempFile
-//            // test.h2.db
-//            // test.lock.db 
-//            // test.trace.db 
-//            for(String ext:new String[] {".mv.db",".newFile",".tempFile",".h2.db",".lock.db",".trace.db"}) {
-//                File dbfile=new File(h2gisFilename+ext);
-//                if(dbfile.exists()){ 
-//                    File file = new File(dbfile.getAbsolutePath());
-//                    file.delete();
-//                }
-//            }
-//        }
-//        
-//    }
-//    @Override
-//    public Connection createDbSchema() throws SQLException {
-//        Config config=new Config();
-//        config.setDbfile(h2gisFilename);
-//        config.setDburl(dburl);
-////        H2gisMapping gpkgMapping=new H2gisMapping();
-////        gpkgMapping.preConnect(config.getDburl(),null,null,config);
-////        Connection jdbcConnection=createConnection();
-////        gpkgMapping.postConnect(jdbcConnection, config);
-////        return jdbcConnection;
-//        return null;
-//    }
-//    @Override
-//    public Connection createConnection() throws SQLException {
-//        DriverManager.registerDriver(new org.h2.Driver());
-//        return DriverManager.getConnection(dburl, null, null);
-//    }
-//    @Override
-//    public void initConfig(Config config) {
-//        new ch.ehi.ili2h2gis.H2gisMain().initConfig(config);
-//        if(dbschema!=null) {
-//            config.setDbschema(dbschema);
-//        }
-//    }
-//    @Override
-//    public boolean supportsCompoundGeometry(){
-//        return false;
-//    }
+    private String dbschema;
+    private String duckdbFilename;
+    private String dburl;
+    
+    public DuckDBTestSetup(String duckdbFilename, String dburl) {
+        this(duckdbFilename,dburl,null);
+    }
+    
+    public DuckDBTestSetup(String duckdbFilename, String dburl, String dbschema) {
+        this.duckdbFilename = duckdbFilename;
+        this.dburl = dburl;
+        this.dbschema = dbschema;
+    }
+    
+    @Override
+    public Config initConfig(String xtfFilename, String logfile) {
+        Config config = new Config();
+        new DuckDBMain().initConfig(config);
+        config.setStrokeArcs(config, Config.STROKE_ARCS_ENABLE);
+        config.setDbfile(duckdbFilename);
+        config.setDburl(dburl);
+        if (dbschema != null) {
+            config.setDbschema(dbschema);
+        }
+        if (logfile != null) {
+            config.setLogfile(logfile);
+        }
+        config.setXtffile(xtfFilename);
+        if (xtfFilename != null && Ili2db.isItfFilename(xtfFilename)) {
+            config.setItfTransferfile(true);
+        }
+        return config;
+    }
+    
+    @Override
+    public String prefixName(String name) {
+        if (dbschema == null) {
+            return name;
+        }
+        return dbschema + "." + name;
+    }
 
+    @Override
+    public String getSchema() {
+        return dbschema;
+    }
+    
+    @Override
+    public void resetDb() throws SQLException {
+        if (dbschema != null) {
+            Connection jdbcConnection = createConnection();
+            try {
+                Statement stmt = jdbcConnection.createStatement();
+                try {
+                    stmt.execute("DROP SCHEMA IF EXISTS " + dbschema + " CASCADE");
+                } finally {
+                    stmt.close();
+                    stmt = null;
+                }
+            } finally {
+                jdbcConnection.close();
+                jdbcConnection = null;
+            }
+        }
+        {
+            // test.duckdb
+            // test.duckdb.wal
+            for (String ext : new String[] { ".duckdb", ".duckdb.wal" }) {
+                File dbfile = new File(duckdbFilename + ext);
+                if (dbfile.exists()) {
+                    File file = new File(dbfile.getAbsolutePath());
+                    file.delete();
+                }
+            }
+        }
+    }
+    
+    @Override
+    public Connection createDbSchema() throws SQLException {
+        Config config=new Config();
+        config.setDbfile(duckdbFilename);
+        config.setDburl(dburl);
+        DuckDBMapping duckdbMapping = new DuckDBMapping();
+        duckdbMapping.preConnect(config.getDburl(), null, null, config);
+        Connection jdbcConnection = createConnection();
+        duckdbMapping.postConnect(jdbcConnection, config);
+        return jdbcConnection;
+    }
+    
+    @Override
+    public Connection createConnection() throws SQLException {
+        DriverManager.registerDriver(new org.duckdb.DuckDBDriver());
+        return DriverManager.getConnection(dburl, null, null);
+    }
+    
+    @Override
+    public void initConfig(Config config) {
+        new DuckDBMain().initConfig(config);
+        config.setStrokeArcs(config, Config.STROKE_ARCS_ENABLE);
+        if (dbschema != null) {
+            config.setDbschema(dbschema);
+        }
+    }
+    
+    @Override
+    public boolean supportsCompoundGeometry() {
+        return false;
+    }
 }
